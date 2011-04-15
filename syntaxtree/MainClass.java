5d1b5a9fd44bae90410f5ea13157f0faad839193
Joel Pettersson <petterssonjoel@gmail.com>
2011-04-15 14:28:43 +0200

    Add semantic actions to all productions in parser.

--- syntaxtree/MainClass.java
+++ syntaxtree/MainClass.java
@@ -6,7 +6,7 @@ public class MainClass {
   public Identifier i1,i2;
   public Statement s;
 
-  public MainClass(Identifier ai1, Identifier ai2, Statement as) {
+  public MainClass(Identifier ai1, Identifier ai2, StatementList as) {
     i1=ai1; i2=ai2; s=as;
   }
 
