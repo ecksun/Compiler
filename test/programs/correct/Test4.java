217378bc4c994b0bbd79e62ab77627666dabb40a
Joel Pettersson <petterssonjoel@gmail.com>
2011-05-08 20:20:57 +0200

    Extend correct test case Test4.

--- test/programs/correct/Test4.java
+++ test/programs/correct/Test4.java
@@ -6,14 +6,19 @@ class Test4 {
 
 }
 
-
 class Test4A {
+
+    Test4B test4B;
 
     public int test() {
         int test1;
         int test2;
-        test1 = this.test1(1, 2, 3);
-        test2 = 100;
+        test1 = this.test1(100000, 200000, 300000);
+        test2 = 999999;
+
+        test4B = new Test4B();
+        System.out.println(test4B.test());
+
         return test1;
     }
 
@@ -21,13 +26,22 @@ class Test4A {
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
-        return 0;
+
+        return 1;
     }
 
     public int test1(int a, int b) {
         System.out.println(a);
         System.out.println(b);
-        return 0;
+        return 2;
+    }
+
+}
+
+class Test4B {
+
+    public int test() {
+        return 1337;
     }
 
 }
