05e3e522ab6bd391e9139109e28bcab9fbb5aaea
Linus Wallgren <linus.wallgren@gmail.com>
2011-05-09 15:59:24 +0200

    Final approach to stacksizevisitor

--- syntaxtree/visitor/StackSizeVisitor.java
+++ syntaxtree/visitor/StackSizeVisitor.java
@@ -36,174 +36,217 @@ import syntaxtree.VarDecl;
 import syntaxtree.While;
 
 public class StackSizeVisitor extends DepthFirstVisitor<Integer> {
+    private int maxSize;
+    private int currentSize;
+
+    private Integer updateSize(int i, int j) {
+        currentSize += i;
+        maxSize = Math.max(currentSize, maxSize);
+        currentSize -= j;
+        return maxSize;
+    }
+
     @Override
     public Integer visit(And n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return maxSize;
     }
 
     @Override
     public Integer visit(ArrayAssign n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return maxSize;
     }
 
     @Override
     public Integer visit(ArrayLength n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return maxSize;
     }
 
     @Override
     public Integer visit(ArrayLookup n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return maxSize;
     }
 
     @Override
     public Integer visit(Assign n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return maxSize;
     }
 
     @Override
     public Integer visit(Block n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return maxSize;
     }
 
     @Override
     public Integer visit(BooleanType n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return maxSize;
     }
 
     @Override
     public Integer visit(Call n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return maxSize;
     }
 
     @Override
     public Integer visit(ClassDeclExtends n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return maxSize;
     }
 
     @Override
     public Integer visit(ClassDeclSimple n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return maxSize;
     }
 
     @Override
     public Integer visit(False n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return maxSize;
     }
 
     @Override
     public Integer visit(Formal n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return maxSize;
     }
 
     @Override
     public Integer visit(Identifier n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return maxSize;
     }
 
     @Override
     public Integer visit(IdentifierExp n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return maxSize;
     }
 
     @Override
     public Integer visit(IdentifierType n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return maxSize;
     }
 
     @Override
     public Integer visit(If n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return maxSize;
     }
 
     @Override
     public Integer visit(IntArrayType n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return maxSize;
     }
 
     @Override
     public Integer visit(IntegerLiteral n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return maxSize;
     }
 
     @Override
     public Integer visit(IntegerType n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return updateSize(0, 0);
     }
 
     @Override
     public Integer visit(LessThan n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return updateSize(1, 1);
     }
 
     @Override
     public Integer visit(MainClass n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return updateSize(0, 0);
     }
 
     @Override
     public Integer visit(MethodDecl n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return updateSize(2, 1);
     }
 
     @Override
     public Integer visit(Minus n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return updateSize(0, 1);
     }
 
     @Override
     public Integer visit(NewArray n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return updateSize(1, 0);
     }
 
     @Override
     public Integer visit(NewObject n) {
-        n.id.accept(this);
-        return super.visit(n) + 0;
+        super.visit(n);
+        return updateSize(2, 1);
     }
 
     @Override
     public Integer visit(Not n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return updateSize(0, 0);
     }
 
     @Override
     public Integer visit(Plus n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return updateSize(0, 1);
     }
 
     @Override
     public Integer visit(Print n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return updateSize(1, 2);
     }
 
     @Override
     public Integer visit(Program n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return updateSize(0, 0);
     }
 
     @Override
     public Integer visit(This n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return updateSize(1, 0);
     }
 
     @Override
     public Integer visit(Times n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return updateSize(0, 1);
     }
 
     @Override
     public Integer visit(True n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return updateSize(1, 0);
     }
 
     @Override
     public Integer visit(VarDecl n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return updateSize(0, 0);
     }
 
     @Override
     public Integer visit(While n) {
-        return super.visit(n) + 0;
+        super.visit(n);
+        return updateSize(0, 1);
     }
 }
