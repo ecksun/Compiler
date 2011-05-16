547aa29d8994f50ee47f6b3b1a1b4af49bf07a64
Joel Pettersson <petterssonjoel@gmail.com>
2011-05-16 12:05:47 +0200

    Add test case for (unary) minus.

---
+++ test/programs/execute/TestMinus.java
@@ -0,0 +1,34 @@
+class TestMinus {
+    public static void main(String[] args) {
+        if (new TestMinusHelper().testMinus()) {} else {}
+    }
+}
+
+class TestMinusHelper {
+    public boolean testMinus() {
+        int x;
+
+        // NOTE: All integers operations are well-defined mod 2^32.
+        x = print(2-1); // subtract the constant 1
+        x = print(2+-1); // add the contant -1
+        x = print(2--1); // subtract the constant -1
+        x = print(2-2147483647); // subtract largest positive int
+        x = print(2+-2147483648); // add largest negative int
+        x = print(2--2147483648); // subtract largest negative int
+
+        // Correct Java for reference output.
+        //x = print(2-1); // subtract the constant 1
+        //x = print(2+(-1)); // add the contant -1
+        //x = print(2-(-1)); // subtract the constant -1
+        //x = print(2-2147483647); // subtract largest positive int
+        //x = print(2+(-2147483648)); // add largest negative int
+        //x = print(2-(-2147483648)); // subtract largest negative int
+
+        return true;
+    }
+
+    public int print(int x) {
+        System.out.println(x);
+        return x;
+    }
+}
